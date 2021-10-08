0071895087251bb31875dd8174f4383dda50703e
BIRIBIET02 <biribiet@mail.ru>
2021-10-08 13:30:58 +0900

    PR

---
+++ app/src/main/java/com/example/prfive02/br.java
@@ -0,0 +1,14 @@
+package com.example.prfive02;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class br extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_br);
+    }
+}
